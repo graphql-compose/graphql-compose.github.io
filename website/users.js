0ee229e72809e89bc7b5b8693b57305ea1a838ab
Paul Damnhorns <pavel.chertorogov@gmail.com>
2021-08-12 01:18:37 +0600

    Update users.js

--- website/users.js
+++ website/users.js
@@ -10,5 +10,23 @@ module.exports = [
     image: 'https://www.graphql-mesh.com/img/mesh-logo.svg',
     infoLink: 'https://www.graphql-mesh.com',
     pinned: true
-  }
+  },
+  {
+    caption: '@neo4j/graphql',
+    image: 'https://dist.neo4j.com/wp-content/uploads/20210423072428/neo4j-logo-2020-1.svg',
+    infoLink: 'https://github.com/neo4j/graphql/tree/master/packages/graphql',
+    pinned: true
+  },
+  {
+    caption: 'Directus',
+    image: 'https://directus.io/assets/favicon.svg',
+    infoLink: 'https://directus.io/',
+    pinned: true
+  },
+  {
+    caption: 'Gridsome',
+    image: 'https://gridsome.org/logos/logo-normal.svg',
+    infoLink: 'https://gridsome.org/',
+    pinned: true
+  } 
 ];
